5815204648e0c1c4d8f8fdf50dd1f51c674ddce4
druatta <druatta@ucsc.edu>
2015-01-10 14:42:38 -0800

    Update User.java

--- hackucsc/app/src/main/java/User/User.java
+++ hackucsc/app/src/main/java/User/User.java
@@ -6,25 +6,50 @@ import java.util.HashMap;
  * Created by Daniel and Cris on 1/9/2015.
  */
 public class User {
-    private String fullName;
-    private String make;
-    private String model;
-    private boolean driver;
-    private int minute;
-    private int hour;
-    //TBD: Current location
-    //TBD: Route
-    private int seats;
-    private String[] reviews;
+    private String email; // static
+    private String fullName; // static
+    private String make; // dynamic
+    private String model; // dynamic
+    private String color; // dynamic
+    private boolean driver; // dynamic
+    private int minute; // dynamic
+    private int hour; // dynamic
+    //TBD: Current location, to be pulled from maps, dynamic
+    //TBD: Route, to be pulled from maps, dynamic
+    private int seats; // dynamic
+    private String[] reviews; // dynamic. How can multiple users access this?
     //TBD: payment method
-    private boolean driving;
-    private String address;
-    private boolean toCampus;
-    private int phone;
+    private boolean driving; // dynamic
+    private String address; // dynamic
+    private boolean toCampus; // dynamic. Incomplete
+    private int phone; // dynamic
 
-    public User(String fullName) {
-        this.fullName = fullName;
+    public User(String email, int phone) { // Passenger Account creation
+        this.email = email;
+        // this.fullName TBD - to be pulled from facebook
+        this.phone = phone;
+        this.driver = false;
     }
+
+    public User(String email, int phone, String make, String model, String color) { // Driver Account creation
+        this.email = email;
+        // this.fullName TBD - to be pulled from facebook
+        this.phone = phone;
+        this.driver = true;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+    }
+
+    public User() {};
+
+    public String getEmail() {return this.email; }
+
+    public void setEmail(String email) {this.email = email; }
+
+    public String getFullName() { return this.fullName; }
+
+    // Note: Don't need fullName setter, will pull from facebook
 
     public String getMake() {
         return this.make;
@@ -41,6 +66,10 @@ public class User {
     public void setModel(String model) {
         this.model = model;
     }
+
+    public String getColor() { return this.color; }
+
+    public void setColor(String color) { this.color = color; }
 
     public boolean getDriver() {
         return driver;
@@ -102,6 +131,10 @@ public class User {
         return this.toCampus;
     }
 
+    public void setToCampus(boolean toCampus) {
+        this.toCampus = toCampus;
+    }
+
     public void setPhone(int phone) {
         this.phone = phone;
     }
@@ -109,10 +142,6 @@ public class User {
     public int getPhone() {
         return this.phone;
     }
-
-    public User() {};
-
-
 
 
 }
